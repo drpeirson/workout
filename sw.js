1193bfbc25d840abd273ee52c0e9c4cd3bfdaccf
Jack Peirson <jackpeirson@gmail.com>
2025-12-09 23:58:09 +0000

    Update sw.js
    optimisation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* MST Workout Tracker - Service Worker */
-const CACHE_NAME = "bolt-cache-v7";
+const CACHE_NAME = "bolt-cache-v8"; // Version bumped to force update
 
 // App shell (keep this small)
 const CORE_ASSETS = [
